3c8c3f07e06ca44831aac0ce631ab6ab3ef1d092
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-02-18 12:42:25 +0100

    added inference support

---
+++ Detection/data_loader.py
@@ -0,0 +1,124 @@
+from pathlib import Path
+from SlideRunner.dataAccess.database import Database
+import openslide
+
+
+from fastai import *
+from fastai.vision import *
+from fastai.callbacks import *
+
+class SlideContainer():
+
+    def __init__(self, file: Path, level: int=0, width: int=256, height: int=256):
+        self.file = file
+        self.slide = openslide.open_slide(str(file))
+        self.width = width
+        self.height = height
+        self.down_factor = self.slide.level_downsamples[level]
+
+        if level is None:
+            level = self.slide.level_count - 1
+        self.level = level
+
+    def get_patch(self,  x: int=0, y: int=0):
+        return np.array(self.slide.read_region(location=(int(x * down_factor),int(y * down_factor)),
+                                          level=self.level, size=(self.width, self.height)))[:, :, :3]
+
+    @property
+    def shape(self):
+        return (self.width, self.height)
+
+    def __str__(self):
+        return str(self.path)
+
+def bb_pad_collate(samples:BatchSamples, pad_idx:int=0) -> Tuple[FloatTensor, Tuple[LongTensor, LongTensor]]:
+    "Function that collect `samples` of labelled bboxes and adds padding with `pad_idx`."
+    samples = [s for s in samples if s[1].data[0].shape[0] > 0] # check that labels are available
+
+    max_len = max([len(s[1].data[1]) for s in samples])
+    bboxes = torch.zeros(len(samples), max_len, 4)
+    labels = torch.zeros(len(samples), max_len).long() + pad_idx
+    imgs = []
+    for i,s in enumerate(samples):
+        imgs.append(s[0].data[None])
+        bbs, lbls = s[1].data
+        bboxes[i,-len(lbls):] = bbs
+        labels[i,-len(lbls):] = torch.from_numpy(lbls)
+    return torch.cat(imgs,0), (bboxes,labels)
+
+class SlideLabelList(LabelList):
+
+    def __getitem__(self,idxs:Union[int,np.ndarray])->'LabelList':
+        idxs = try_int(idxs)
+        if isinstance(idxs, numbers.Integral):
+            if self.item is None:
+                h, w = self.x.items[idxs].shape
+                class_id = np.random.choice(list(set(self.y.items[idxs][1])), 1)[0]
+                ids = self.y.items[idxs][1] == class_id
+                xmin, ymin, xmax, ymax = np.array(self.y.items[idxs][0])[ids][randint(0, np.count_nonzero(ids) - 1)]
+                x = self.x.get(idxs, int(xmin - w / 2), int(ymin - h / 2))
+                y = self.y.get(idxs, int(xmin - w / 2), int(ymin - h / 2))
+            else:
+                x,y = self.item ,0
+            if self.tfms or self.tfmargs:
+                x = x.apply_tfms(self.tfms, **self.tfmargs)
+            if hasattr(self, 'tfms_y') and self.tfm_y and self.item is None:
+                y = y.apply_tfms(self.tfms_y, **{**self.tfmargs_y, 'do_resolve':False})
+            if y is None: y=0
+            return x,y
+        else:
+            return self.new(self.x[idxs], self.y[idxs])
+
+
+class SlideItemList(ItemList):
+
+    def __getitem__(self,idxs: int, x: int=0, y: int=0)->Any:
+        idxs = try_int(idxs)
+        if isinstance(idxs, numbers.Integral):
+            return self.get(idxs, x, y)
+        else:
+            return self.get(*idxs)
+
+    def label_from_list(self, labels:Iterator, label_cls:Callable=None, **kwargs)->'LabelList':
+        "Label `self.items` with `labels`."
+        labels = array(labels, dtype=object)
+        label_cls = self.get_label_cls(labels, label_cls=label_cls, **kwargs)
+        y = label_cls(labels, path=self.path, **kwargs)
+        res = SlideLabelList(x=self, y=y)
+        return res
+
+
+class SlideImageItemList(SlideItemList):
+    pass
+
+class SlideObjectItemList(SlideImageItemList, ImageItemList):
+
+    def get(self, i, x: int, y: int):
+        fn = self.items[i]
+        res = self.open(fn, x, y)
+        self.sizes[i] = res.size
+        return res
+
+class ObjectItemListSlide(SlideObjectItemList):
+
+    def open(self, fn: SlideContainer,  x: int=0, y: int=0):
+        return Image(pil2tensor(fn.get_patch(x, y) / 255., np.float32))
+
+
+class SlideObjectCategoryList(ObjectCategoryList):
+
+    def get(self, i, x: int=0, y: int=0):
+        h, w = self.x.items[i].shape
+        bboxes, labels = self.items[i]
+        if x > 0 and y > 0:
+            bboxes = np.array(bboxes)
+            labels = np.array(labels)
+            ids = (bboxes[:, 0] > x) & (bboxes[:, 2] < x + w) & (bboxes[:, 1] > y) & (bboxes[:, 3] < y + h)
+            bboxes = [[box[1] - y,box[0] - x, box[3] - y,  box[2] - x] for box in bboxes[ids]]
+            labels = labels[ids] # list(labels)
+            return ImageBBox.create(h, w, bboxes, labels, classes=self.classes, pad_idx=self.pad_idx)
+        else:
+            # just taking 10 boxes speed up initalisation
+            return ImageBBox.create(h, w, bboxes[:10], labels[:10], classes=self.classes, pad_idx=self.pad_idx)
+
+
