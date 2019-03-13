67fa5ef6fee352edbdc643951fad94ec5cc46067
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-03-13 20:34:48 +0100

    Added new function to use a custom data sampling method

--- Detection/data_loader.py
+++ Detection/data_loader.py
@@ -1,3 +1,4 @@
+import numpy as np
 from pathlib import Path
 from SlideRunner.dataAccess.database import Database
 import openslide
@@ -13,12 +14,15 @@ from fastai.data_block import *
 
 class SlideContainer():
 
-    def __init__(self, file: Path, level: int=0, width: int=256, height: int=256):
+    def __init__(self, file: Path, y, level: int=0, width: int=256, height: int=256, sample_func: callable=None):
         self.file = file
         self.slide = openslide.open_slide(str(file))
         self.width = width
         self.height = height
         self.down_factor = self.slide.level_downsamples[level]
+        self.y = y
+        self.sample_func = sample_func
+        self.classes = list(set(self.y[1]))
 
         if level is None:
             level = self.slide.level_count - 1
@@ -34,6 +38,19 @@ class SlideContainer():
 
     def __str__(self):
         return str(self.path)
+
+    def get_new_train_coordinates(self):
+        # use passed sampling method
+        if callable(self.sample_func):
+            return self.sample_func(self.y, **{"classes": self.classes, "size": self.shape,
+                                               "level_dimensions": self.slide.level_dimensions,
+                                               "level": self.level})
+
+        # use default sampling method
+        class_id = np.random.choice(self.classes, 1)[0]
+        ids = self.y[1] == class_id
+        xmin, ymin, _, _ = np.array(self.y[0])[ids][randint(0, np.count_nonzero(ids) - 1)]
+        return int(xmin - self.shape / 2), int(ymin - self.height / 2)
 
 def bb_pad_collate_min(samples:BatchSamples, pad_idx:int=0) -> Tuple[FloatTensor, Tuple[LongTensor, LongTensor]]:
     "Function that collect `samples` of labelled bboxes and adds padding with `pad_idx`."
@@ -54,33 +71,15 @@ class SlideLabelList(LabelList):
 
 
     def __getitem__(self,idxs:Union[int,np.ndarray])->'LabelList':
-        _bg_label_prob = 0.1 # add a backgound label to sample complete random
-        _random_offset_scale = 0.5 # up to 50% offset to left and right of frame
         idxs = try_int(idxs)
         if isinstance(idxs, numbers.Integral):
             if self.item is None:
-                h, w = self.x.items[idxs].shape
-                smaple_probability = np.histogram(self.y.items[idxs][1], bins=list(range(0, self.c+1)), density=True)[0]
-                non_zero_ids = (smaple_probability > 0) & (smaple_probability < 1)
-                smaple_probability[non_zero_ids] = 1 - smaple_probability[non_zero_ids]
-                smaple_probability[0] = _bg_label_prob
-                # softmax
-                non_zero_ids = smaple_probability > 0
-                xoffset=randint(-w,w)*_random_offset_scale
-                yoffset=randint(-h,h)*_random_offset_scale
-                smaple_probability[non_zero_ids] = np.exp(smaple_probability[non_zero_ids]) \
-                                                   / np.sum(np.exp(smaple_probability[non_zero_ids]), axis=0)
-                class_id = np.random.choice(list(range(0, self.c)), 1, p=smaple_probability)[0]
-                if class_id == 0:
-                    level = self.x.items[idxs].level
-                    slide_width, slide_height = self.x.items[idxs].slide.level_dimensions[level]
-                    xmin, ymin = randint(int(w / 2 -xoffset), slide_width - w), randint(int(h / 2-yoffset), slide_height - h)
-                else:
-                    # sample from a completely random image position
-                    ids = self.y.items[idxs][1] == class_id
-                    xmin, ymin, xmax, ymax = np.array(self.y.items[idxs][0])[ids][randint(0, np.count_nonzero(ids) - 1)]
-                x = self.x.get(idxs, int(xmin - w / 2 + xoffset), int(ymin - h / 2 +yoffset))
-                y = self.y.get(idxs, int(xmin - w / 2 + xoffset), int(ymin - h / 2 +yoffset))
+                slide_container = self.x.items[idxs]
+
+                xmin, ymin = slide_container.get_new_train_coordinates()
+
+                x = self.x.get(idxs, xmin, ymin)
+                y = self.y.get(idxs, xmin, ymin)
             else:
                 x,y = self.item ,0
             if self.tfms or self.tfmargs:
