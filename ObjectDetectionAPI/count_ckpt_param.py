ad7508444e368af3aa1cc578b43d57013b5ac705
c.marzahl@de.eu.local <c.marzahl@euroimmun.de>
2019-05-15 08:53:29 +0200

    Added RCNN and SSD

---
+++ ObjectDetectionAPI/count_ckpt_param.py
@@ -0,0 +1,19 @@
+import sys
+import tensorflow as tf
+import numpy as np
+
+ckpt_fpath = "model_ssd_mobilenet_v2/train/baseline_300/model.ckpt-0"
+
+# Open TensorFlow ckpt
+reader = tf.train.NewCheckpointReader(ckpt_fpath)
+
+print('\nCount the number of parameters in ckpt file(%s)' % ckpt_fpath)
+param_map = reader.get_variable_to_shape_map()
+total_count = 0
+for k, v in param_map.items():
+    if 'Momentum' not in k and 'global_step' not in k:
+        temp = np.prod(v)
+        total_count += temp
+        print('%s: %s => %d' % (k, str(v), temp))
+
+print('Total Param Count: %d' % total_count)
